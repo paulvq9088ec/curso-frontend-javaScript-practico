ce5c19770a652eab2112902f0037645268fe7200
Paul Veintimilla <paulvq9088@hotmail.com>
2022-07-28 21:58:34 -0500

    Agregamos mobileMenu y el asideMenu al proyecto

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
-const menuEmail = document.querySelector('.navbar-shopping-cart');
+const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', togglemobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
+    const isCarritoClosed = aside.classList.contains('inactive');
+    if(!isCarritoClosed){
+        aside.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
+
+function togglemobileMenu(){
+    const isCarritoClosed = aside.classList.contains('inactive');
+
+    if(!isCarritoClosed){
+        aside.classList.add('inactive');
+    }
+    
+    mobileMenu.classList.toggle('inactive');
+
+}
+
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDestopMenuClosed = desktopMenu.classList.contains('inactive');
+    
+    
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    
+    if(!isDestopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+
+}
+
